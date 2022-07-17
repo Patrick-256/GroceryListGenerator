819fd1236a6676d9fc6066b0f2420cc3ee9de5e9
Patrick W <321buildablock123@gmail.com>
2022-07-17 11:49:47 -0700

    v 0.1.0 UI can display the master grocerylist, and UI can add new items to the master grocery list

--- GroceryListGenerator.js
+++ GroceryListGenerator.js
@@ -9,32 +9,96 @@ const app = express();
 
 //Run the program
 runMainProgram();
+//runTestProgram();
 
 async function runMainProgram()
 {
     //Load master grocery list from file
-    var groceryItemsMasterList = await readJSONfileAsync('public/groceryItems.json');
+    var groceryItemsMasterList = await readJSONfileAsync('programData/groceryItems.json');
 
     //Start the web server
     app.listen(3257, ()=> console.log('Grocery List Generator server is starting!'));
     app.use(express.static('public'))
 
-    //return master grocerylist to client
+    
+    //Program file request  //return master grocerylist to client
+    app.get('/ProgramFileRequest/:fileName?', getProgramFile);
+
+    async function getProgramFile(req, res) {
+        var data = req.params;
+        var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress || null;
+
+        console.log(`USER IP: ${ip} is requesting fileName: ${data.fileName}`)
+
+        reply = await readJSONfileAsync(`programData/${data.fileName}.json`)
+        res.send(reply);
+    }
 
     //add new grocery item from client
+    app.get('/addNewGroceryItemToMaster/:newItemName/:category/:store/:cost/:amount/:unit/:description?', addNewGroceryItemToMaster);
 
-    //modify a grocery item from client
+    async function addNewGroceryItemToMaster(req,res) {
+        var data = req.params;
+        console.log(`adding ${data.newItemName} to master grocery list!`)
+
+        groceryItemsMasterList[data.category][data.newItemName] = 
+        {
+            needed: false,
+            acquiredFrom: {}
+        };
+        groceryItemsMasterList[data.category][data.newItemName].acquiredFrom[data.store] = {};
+        groceryItemsMasterList[data.category][data.newItemName].acquiredFrom[data.store].description = data.description;
+        groceryItemsMasterList[data.category][data.newItemName].acquiredFrom[data.store].cost = data.cost;
+        groceryItemsMasterList[data.category][data.newItemName].acquiredFrom[data.store].forAmount = data.amount;
+        groceryItemsMasterList[data.category][data.newItemName].acquiredFrom[data.store].unit = data.unit;
+
+        //save master grocery list
+        await WriteFilePromise('programData/groceryItems.json',JSON.stringify(groceryItemsMasterList,null,4))
+
+        res.send(`added ${data.newItemName} to master grocery list!`)
+    }
+
+    //add another store to a grocery item - can also modify an existing store
+    
 
     //delete a grocery item from client
 }
 
 
+async function runTestProgram() {
+    //load grocery list file
+    var groceryItemsMasterList = await readJSONfileAsync('programData/groceryItemsV2.json');
 
+    //add a new item
+    var data = {
+        "newItemName":"refried_beans",
+        "category":"canned_goods",
+        "store":"ralphs",
+        "description":"1_can_of_beans",
+        "cost":Number(1.49),
+        "amount":Number(16),
+        "unit":"oz" 
+    }
+
+    groceryItemsMasterList[data.category][data.newItemName] = 
+    {
+        needed: false,
+        acquiredFrom: {}
+    };
+    groceryItemsMasterList[data.category][data.newItemName].acquiredFrom[data.store] = {};
+    groceryItemsMasterList[data.category][data.newItemName].acquiredFrom[data.store].description = data.description;
+    groceryItemsMasterList[data.category][data.newItemName].acquiredFrom[data.store].cost = data.cost;
+    groceryItemsMasterList[data.category][data.newItemName].acquiredFrom[data.store].forAmount = data.amount;
+    groceryItemsMasterList[data.category][data.newItemName].acquiredFrom[data.store].unit = data.unit;
+
+    //save
+    await WriteFilePromise('programData/groceryItemsV2.json',JSON.stringify(groceryItemsMasterList,null,4))
+}
 
 
 
 //Needed functions
-function readJSONfileAsync(fileName)
+async function readJSONfileAsync(fileName)
 {
     return new Promise((resolve,reject) => {
         var dataObj = {};
@@ -43,13 +107,15 @@ function readJSONfileAsync(fileName)
         try
         {
             fs.readFile(fName, 'utf8', function(err, data) {
+                console.log(`Data read from ${fName}: `);
+                console.log(data);
                 dataObj = JSON.parse(data);
                 resolve(dataObj);
             })
         }
         catch(err)
         {
-            console.log(err) //createTickEvent(TickData,"CoinPair","readJSONfile (async)","error","Error! : "+err)
+            console.log(err) 
             reject(err)
         }
     });
--- public/index.js
+++ public/index.js
@@ -1,6 +1,165 @@
-function loadGroceryList()
+async function loadGroceryList()
 {
     //ask server for the grocery list
+    var MasterGroceryList = await getJSONfileAsync("groceryItems")
+
+    //console.log(`test Variable: cost of first item from first store: ${MasterGroceryList.groceryListArray[0].acquiredFrom[0].cost} `)
 
     //now draw it on the screen
+    //Draw it on the master list, if its currently needed, add it to the current grocery list
+
+    for(const cate in MasterGroceryList)
+    {
+        //make the category header
+        console.log(`${cate}: ${MasterGroceryList[cate]}`);
+        var newCategoryEl = document.createElement("div");
+        newCategoryEl.className = "category";
+        newCategoryEl.textContent = cate;
+        document.getElementById("masterListDisplay").appendChild(newCategoryEl)
+
+
+        //for each category, list out its items
+        for(const item in MasterGroceryList[cate])
+        {
+            //parent element for the master list
+            var newGroceryItem = document.createElement("div")
+            newGroceryItem.className = "groceryItem";
+            console.log(`${item}: ${MasterGroceryList[cate][item]}`);
+
+            //item needed checkbox
+            var newGroceryItemNeeded = document.createElement("div")
+            newGroceryItemNeeded.className = "groceryItemNeeded";
+            if(MasterGroceryList[cate][item].needed == true) {
+                newGroceryItemNeeded.textContent = ":)";
+                newGroceryItemNeeded.style.backgroundColor = "green";
+            } else {
+                newGroceryItemNeeded.textContent = "-_-"
+                newGroceryItemNeeded.style.backgroundColor = "blue";
+            }
+            newGroceryItem.appendChild(newGroceryItemNeeded);
+
+            //item name
+            var newGroceryItemName = document.createElement("div")
+            newGroceryItemName.className = "groceryItemName";
+            newGroceryItemName.textContent = item;
+            newGroceryItem.appendChild(newGroceryItemName);
+
+
+            //for each item, list out its stores
+            var newGroceryItemAcquireFromArray = document.createElement("div")
+            newGroceryItemAcquireFromArray.className = "groceryItemAcquireFromArray";
+
+            for(const stor in MasterGroceryList[cate][item].acquiredFrom)
+            {
+                console.log(`${stor}: ${MasterGroceryList[cate][item].acquiredFrom[stor]}`);
+
+                var newGroceryItemAcquiredFromStore = document.createElement("div");
+                newGroceryItemAcquiredFromStore.className = "groceryItemAcquireFromStore";
+
+                //store name
+                var newGroceryItemAcquiredFromStoreName = document.createElement("div");
+                newGroceryItemAcquiredFromStoreName.className = "groceryItemAcquireFromStoreName";
+                newGroceryItemAcquiredFromStoreName.textContent = stor;
+                newGroceryItemAcquiredFromStore.appendChild(newGroceryItemAcquiredFromStoreName);
+
+                //description
+                var newGroceryItemAcquiredFromStoreDescription = document.createElement("div");
+                newGroceryItemAcquiredFromStoreDescription.className = "groceryItemAcquireFromStoreDescription";
+                newGroceryItemAcquiredFromStoreDescription.textContent = MasterGroceryList[cate][item].acquiredFrom[stor].description;
+                newGroceryItemAcquiredFromStore.appendChild(newGroceryItemAcquiredFromStoreDescription);
+
+                //store price
+                var newGroceryItemAcquiredFromStorePrice = document.createElement("div");
+                newGroceryItemAcquiredFromStorePrice.className = "groceryItemAcquireFromStorePrice";
+                newGroceryItemAcquiredFromStorePrice.textContent = MasterGroceryList[cate][item].acquiredFrom[stor].cost;
+                newGroceryItemAcquiredFromStore.appendChild(newGroceryItemAcquiredFromStorePrice);
+
+                //store amount
+                var newGroceryItemAcquiredFromStoreAmount = document.createElement("div");
+                newGroceryItemAcquiredFromStoreAmount.className = "groceryItemAcquireFromStoreAmount";
+                newGroceryItemAcquiredFromStoreAmount.textContent = MasterGroceryList[cate][item].acquiredFrom[stor].forAmount;
+                newGroceryItemAcquiredFromStore.appendChild(newGroceryItemAcquiredFromStoreAmount);
+
+                //store unit
+                var newGroceryItemAcquiredFromStoreUnit = document.createElement("div");
+                newGroceryItemAcquiredFromStoreUnit.className = "groceryItemAcquireFromStoreUnit";
+                newGroceryItemAcquiredFromStoreUnit.textContent = MasterGroceryList[cate][item].acquiredFrom[stor].unit;
+                newGroceryItemAcquiredFromStore.appendChild(newGroceryItemAcquiredFromStoreUnit);
+
+
+                //add this store to the array of stores
+                newGroceryItemAcquireFromArray.appendChild(newGroceryItemAcquiredFromStore);
+
+            }
+            newGroceryItem.appendChild(newGroceryItemAcquireFromArray);
+            //Add this item to the master list
+            document.getElementById("masterListDisplay").appendChild(newGroceryItem)
+        }
+        
+    }
+}
+
+//Add a new item the master grocery list
+function addNewItemButtonHasBeenClicked()
+{
+    //get the input data
+    var newItemName = document.getElementById("addNewItemName").value;
+    var newItemCategory = document.getElementById("addNewItemCategory").value;
+    var newItemStore = document.getElementById("addNewItemStore").value;
+    var newItemCost = document.getElementById("addNewItemCost").value;
+    var newItemAmount = document.getElementById("addNewItemAmount").value;
+    var newItemUnit = document.getElementById("addNewItemUnit").value;
+    var newItemDescription = document.getElementById("addNewItemDescription").value;
+
+    //communicate with the server to add the new item
+    var IOlink = `/addNewGroceryItemToMaster/${newItemName}/${newItemCategory}/${newItemStore}/${newItemCost}/${newItemAmount}/${newItemUnit}/${newItemDescription}?`;
+    document.location = IOlink;
+}
+
+//add grocery item to current list
+function addGroceryItemToCurrentList()
+{
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+async function getJSONfileAsync(fileName)
+{
+    return new Promise((resolve,reject) => {
+        try
+        {
+            dataObjLink = `/ProgramFileRequest/${fileName}?`;
+
+            var xhr = new XMLHttpRequest();  
+            xhr.open('GET', dataObjLink, true);
+            xhr.onload = function() {
+                var dataObj = JSON.parse(this.responseText);
+
+                resolve(dataObj);
+            }
+            xhr.send();
+        }
+        catch(err)
+        {
+            console.log(err)
+            reject(err)
+        }
+    });
 }
